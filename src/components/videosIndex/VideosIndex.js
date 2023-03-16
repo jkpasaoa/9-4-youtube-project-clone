eea21a94af98c9b41408db91e4b198de77afb6c9
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-03-16 00:22:01 -0400

    videoindex changes, thumbnails to video functionality

--- src/components/videosIndex/VideosIndex.js
+++ src/components/videosIndex/VideosIndex.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { getAllVids } from "../../api/fetch"
 import Video from "./Video"
+import YouTube from "react-youtube"
 
 export default function VideosIndex({ searchInput, setSearchInput, searchTitle, setSearchTitle }) {
 
@@ -19,6 +20,18 @@ export default function VideosIndex({ searchInput, setSearchInput, searchTitle, 
             })
     }, [searchTitle])
 
+       // Options from the react-youtube package
+       const opts = {
+        height: "390",
+        width: "640",
+        playerVars: {
+          autoplay: 0,
+          controls: 1, 
+          // Adds default YouTube player controls (play, pause, stop, fast forward, rewind)
+        },
+      };
+
+
     return (
         <div>
             {
@@ -32,7 +45,9 @@ export default function VideosIndex({ searchInput, setSearchInput, searchTitle, 
                     return (
                         <div key={vidId} className="videos">
                             <Link to={`/videos/${vidId}`}>
-                                <img src={vidThumbnail} />
+                            <YouTube videoId={vidId} opts={opts} /> 
+                            {/* add youtube options here, commented out thumbnail */}
+                                {/* <img src={vidThumbnail} /> */}
                                 <div>{vidTitle}</div>
                                 <div>{vidChannel}</div>
                             </Link>
