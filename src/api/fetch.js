25c5310628ee897dc2c4eb74f18f92111002efdf
SuperNinjaEv <evan0210@gmail.com>
2023-03-19 18:08:59 -0400

    Corrected fetch, BASE_URL causing issues on deployment

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,9 +1,8 @@
 
-const BASE_URL = process.env.REACT_APP_BASE_URL
 const KEY = process.env.REACT_APP_API_KEY
 
 export async function getAllVids(searchTerm, results) {
-    const response = await fetch(`${BASE_URL}?q=${searchTerm}&part=snippet&maxResults=${results}&key=${KEY}`);
+    const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${searchTerm}&part=snippet&maxResults=${results}&key=${KEY}`);
     const data = await response.json();
     return data;
 }
